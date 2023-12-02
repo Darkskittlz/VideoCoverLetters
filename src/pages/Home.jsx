efbe7f306056b07c355bd2f4659ec9164a9eba88
Darkskittlz (Linux) <darkskiiittles@gmail.com>
2023-12-01 19:33:42 -0500

    Video Cover Letter Init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import styled from "styled-components"
+import CoverVideo from "../assets/CoverVideo.mp4"
 
 const GridContainer = styled.div` 
   display: flex;
   justify-content: center;
   flex-direction: column;
-  grid-gap: 20px;
 `
 
 const Home = () => {
   return (
     <GridContainer>
-      <div>This is a test</div>
+      <h1>Introduction: Tristan Neal</h1>
+      <video src={CoverVideo} controls />
     </GridContainer>
   )
 }
